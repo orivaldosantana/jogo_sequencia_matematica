662d1865cc9d0d2a9aa89c13276ddd9e68c8e771
Orivaldo Santana Jr <orivaldo@gmail.com>
2021-03-24 10:30:02 -0300

    Tela básica de instruções

---
+++ codigo_principal.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(500, 500);
+
+}
+
+function draw() {
+    background(220);
+    textSize(36);
+    fill(10);
+    text("Instruções", 150, 70);
+    textSize(16);
+    text("Primeira instrução: use o mouse para selecionar as opções. ", 20, 120, 460)
+}
